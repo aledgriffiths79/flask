c1d88c6a8279c4e7e4cc64d710e165e1965d1be8
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-07 18:59:27 +0100

    first commit

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/about")
+def about():
+  data = []
+  with open("data/company.json", "r")  as json_data:
+    data = json.load(json_data)
+  return render_template("about.html", page_title="About", company=data) 
+  # The "list_of_numbers=[1, 2, 3]"" was used initially after page_title="About" to see a for loop working. Just a test that it works, no other meaning
+  #list_of_numbers=[1, 2, 3])
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html", page_title="Contact")
+
+@app.route("/careers")
+def careers():
+  return render_template("careers.html", page_title="Careers")
+
+if __name__ == "__main__":
+
+  # in cloud 9 i would add the following for the flask app to work:
+  # app.run(host=os.environ.get("IP"),port-int(os.environ.get("PORT")),debug=True)
+  app.run(debug=True)
