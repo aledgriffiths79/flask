2ff3c7608c817130c4c1bf753d232076a8581f14
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-12 18:58:50 +0100

    adding procfile

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 @app.route("/")
 def index():
@@ -33,7 +34,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
   if request.method == "POST":
-    print(request.form)
+    flash("Thanks {}, we have received your message!".format
+          (request.form["name"]))
 
   return render_template("contact.html", page_title="Contact")
 
@@ -44,5 +46,5 @@ def careers():
 if __name__ == "__main__":
 
   # in cloud 9 i would add the following for the flask app to work:
-  # app.run(host=os.environ.get("IP"),port-int(os.environ.get("PORT")),debug=True)
-  app.run(debug=True)
+  app.run(host=os.environ.get("IP"),port-int(os.environ.get("PORT")),debug=True)
+  # app.run(debug=True)
