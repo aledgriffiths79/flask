a02dc258e9be17c355d120354bd72096af4f6bd1
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-14 17:44:19 +0100

    uncommented a syntax thats used locally

--- run.py
+++ run.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
   # in cloud 9 i would add the following for the flask app to work:
   # app.run(host=os.environ.get("IP"),port=int(os.environ.get("PORT")),debug=True)
   # below syntax is for opening project locally not production (heroku)
-  app.run(debug=True) 
+  # app.run(debug=True) 
 
   #  app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', '8080')), debug=True)
 
