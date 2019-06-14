9e0e15c33c9cea373abca458d909de01ed2f71ab
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-14 12:44:46 +0100

    changed syntax again

--- run.py
+++ run.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
 
   #  app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', '8080')), debug=True)
 
-  app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get('PORT', '8080')), debug=True) 
+  # app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get('PORT', '8080')), debug=True)
+  app.run(host=os.getenv('IP'), port=int(os.getenv('PORT'))) 
 
 
